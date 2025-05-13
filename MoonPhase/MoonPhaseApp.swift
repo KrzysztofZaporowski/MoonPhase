b1e0fac54ed967fb4ce29f614b9ad376685e7c6c
KrzysztofZaporowski <krzysztofzaporowski@gmail.com>
2025-05-13 09:38:26 +0200

    Initial Commit

---
+++ MoonPhase/MoonPhaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonPhaseApp.swift
+//  MoonPhase
+//
+//  Created by Krzysztof Zaporowski on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonPhaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
